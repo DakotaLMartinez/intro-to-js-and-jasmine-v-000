0b2bb28441b67da084734de254df5f45c2e9876a
Dakota Martinez <dakotaleemusic@gmail.com>
2016-04-27 01:50:56 +0000

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(iceCream) {
+    return "I love " + iceCream;
+}
+
+function shouting(phrase) {
+    return phrase.toUpperCase();
+}
+
+function roundDown(number) {
+    return Math.floor(number);
+}
+
+function theTruth() {
+    return true;
+}
